f611cba43722f21ebe234203c8de548924452c80
Lucre <josielindamaldonado@gmail.com>
2022-05-13 21:50:03 -0400

    grabbed format and search terms from url location, split, and fetched data from api.

--- assets/js/search-results.js
+++ assets/js/search-results.js
@@ -1,7 +1,31 @@
-console.log('hello')
+var getQueryString = function () {
+  var queryString = document.location.search;
+  console.log(queryString);
+  var queryFormat = queryString.split("=")[2];
+  console.log(queryFormat);
+  var querySearch = queryString.split("&")[0].split("=")[1];
+  console.log(querySearch);
+  getApiCall(queryFormat, querySearch);
+};
 
-$('#backBtn').on('click', function(event){
-    event.preventDefault();
-    console.log('clicked');
-    location.replace('index.html');
-})
+var getApiCall = function (format, search) {
+  var apiUrl =
+    "https://www.loc.gov/" + format + "/?q=" + search + "&fo=json&c=10";
+  console.log(apiUrl);
+
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        console.log(data);
+      });
+    }
+  });
+};
+
+$("#backBtn").on("click", function (event) {
+  event.preventDefault();
+  console.log("clicked");
+  location.replace("index.html");
+});
+
+getQueryString();
