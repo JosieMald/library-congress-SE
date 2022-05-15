3e1f405d2704be73f4b1e030391f3849ea02af5a
Lucre <josielindamaldonado@gmail.com>
2022-05-15 10:57:43 -0400

    fetched data and console log.

--- assets/js/search-results.js
+++ assets/js/search-results.js
@@ -1,22 +1,28 @@
 var getQueryString = function () {
   var queryString = document.location.search;
-  console.log(queryString);
+//   console.log(queryString);
   var queryFormat = queryString.split("=")[2];
-  console.log(queryFormat);
+//   console.log(queryFormat);
   var querySearch = queryString.split("&")[0].split("=")[1];
-  console.log(querySearch);
+//   console.log(querySearch);
   getApiCall(queryFormat, querySearch);
 };
 
 var getApiCall = function (format, search) {
   var apiUrl =
     "https://www.loc.gov/" + format + "/?q=" + search + "&fo=json&c=10";
-  console.log(apiUrl);
+//   console.log(apiUrl);
 
   fetch(apiUrl).then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
         console.log(data);
+        console.log(data.results[0])
+        console.log("title: " + data.results[0].title)
+        console.log("date: " + data.results[0].date)
+        console.log("subject: " + data.results[0].subject[0])
+        console.log("description: " + data.results[0].description)
+        console.log("link: " + data.results[0].url)
       });
     }
   });
