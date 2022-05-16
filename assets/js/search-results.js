56d14af4229458d4666fcd54e930cf2d3baa8502
Lucre <josielindamaldonado@gmail.com>
2022-05-16 09:35:19 -0400

    removed hardcoded data from html and dynamically displayed api data on page.

--- assets/js/search-results.js
+++ assets/js/search-results.js
@@ -1,31 +1,39 @@
+var containerEl = $('.dataContainer');
+
+
 var getQueryString = function () {
   var queryString = document.location.search;
-//   console.log(queryString);
   var queryFormat = queryString.split("=")[2];
-//   console.log(queryFormat);
   var querySearch = queryString.split("&")[0].split("=")[1];
-//   console.log(querySearch);
   getApiCall(queryFormat, querySearch);
 };
 
 var getApiCall = function (format, search) {
   var apiUrl =
     "https://www.loc.gov/" + format + "/?q=" + search + "&fo=json&c=10";
-//   console.log(apiUrl);
 
   fetch(apiUrl).then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
-        console.log(data);
-        console.log(data.results[0])
-        console.log("title: " + data.results[0].title)
-        console.log("date: " + data.results[0].date)
-        console.log("subject: " + data.results[0].subject[0])
-        console.log("description: " + data.results[0].description)
-        console.log("link: " + data.results[0].url)
+        displayData(data);
       });
     }
   });
+};
+
+var displayData = function (data) {
+    $('.showResults').text("Showing results for " + data.search.query)
+    for (var i = 0; i < data.results.length; i++) {
+        $('.dataContainer').append(
+            "<div class='m-4 col-12 w-auto hello'>" +
+        '<h2>' + data.results[i].title + '</h2>' +
+        '<p> Date: ' + data.results[i].date + '</p>' +
+        '<p> Subject: ' + data.results[i].subject[0] + '</p>' +
+        '<p> Description: ' + data.results[i].description + '</p>' +
+        "<button type='button' class='btn btn-dark mb-3 read-more'> Read More </button>" + '</div>')
+    }
+  console.log(data);
+  console.log(data.search.query);
 };
 
 $("#backBtn").on("click", function (event) {
