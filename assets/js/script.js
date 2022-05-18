8fbc3044c455ec28eb12fbce73faee6094190c39
Lucre <josielindamaldonado@gmail.com>
2022-05-18 13:54:07 -0400

    added comments, formated all docs, and deleted unnecessary code.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,16 @@
 // DOM ELEMENTS --------------------------------------------------------------
-var searchTermEl = $('#search-term');
-var formatInputEl = $('#format-input');
-var submitBtnEl = $('#submitBtn');
+var searchTermEl = $("#search-term");
+var formatInputEl = $("#format-input");
+var submitBtnEl = $("#submitBtn");
 
 // CLICK EVENTS ---------------------------------------------------------------
 $("#submitBtn").on("click", function (event) {
-    event.preventDefault();
-    searchTerm = searchTermEl.val().trim();
-    formatInput = formatInputEl.val().trim();
-    if(searchTerm === ''){
-        alert('Please enter a search term');
-    }
-    var queryParameters = 'q=' + searchTerm + '&format=' + formatInput
-    location.replace('search-results.html?' + queryParameters);
+  event.preventDefault();
+  searchTerm = searchTermEl.val().trim();
+  formatInput = formatInputEl.val().trim();
+  if (searchTerm === "") {
+    alert("Please enter a search term");
+  }
+  var queryParameters = "q=" + searchTerm + "&format=" + formatInput;
+  location.replace("search-results.html?" + queryParameters);
 });
-
-
--- assets/js/search-results.js
+++ assets/js/search-results.js
@@ -1,8 +1,8 @@
 // DOM ELEMENTS -----------------------------------------------------------------------------
-var containerEl = $('.dataContainer');
-var searchTermEl = $('#search-term');
-var formatInputEl = $('#format-input');
-var submitBtnEl = $('#submitBtn');
+var containerEl = $(".dataContainer");
+var searchTermEl = $("#search-term");
+var formatInputEl = $("#format-input");
+var submitBtnEl = $("#submitBtn");
 
 // FUNCTION CALLS ---------------------------------------------------------------------------
 var getQueryString = function () {
@@ -13,54 +13,64 @@ var getQueryString = function () {
 };
 
 var getApiCall = function (format, search) {
-  if (format == 'Select a format...') {
-    format = 'newspapers'
+  if (format == "Select a format...") {
+    format = "newspapers";
   }
   var apiUrl =
     "https://www.loc.gov/" + format + "/?q=" + search + "&fo=json&c=10";
-
 
   fetch(apiUrl).then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
         displayData(data);
       });
-    } 
-});
+    }
+  });
 };
 
 var displayData = function (data) {
-    if (data.results.length == 0) {
-       $('.showResults').text("No results found for  " + data.search.query)
-    } else {
-        $('.showResults').text("Showing results for " + data.search.query)
-    }
-    for (var i = 0; i < data.results.length; i++) {
-        $('.dataContainer').append(
-            "<div class='mx-4 mb-4 col-12 w-auto box'>" +
-        '<h2>' + data.results[i].title + '</h2>' +
-        '<p> Date: ' + data.results[i].date + '</p>' +
-        '<p> Subject: ' + data.results[i].subject[0] + '</p>' +
-        '<p> Description: ' + data.results[i].description + '</p>' +
-        "<a class='btn btn-dark mb-3' href='" + data.results[i].url + "' target='_blank' > Read More </a>" + '</div>')
-    }
-    $('.loading').attr('class', 'hide')
+  if (data.results.length == 0) {
+    $(".showResults").text("No results found for  " + data.search.query);
+  } else {
+    $(".showResults").text("Showing results for " + data.search.query);
+  }
+  for (var i = 0; i < data.results.length; i++) {
+    $(".dataContainer").append(
+      "<div class='mx-4 mb-4 col-12 w-auto box'>" +
+        "<h2>" +
+        data.results[i].title +
+        "</h2>" +
+        "<p> Date: " +
+        data.results[i].date +
+        "</p>" +
+        "<p> Subject: " +
+        data.results[i].subject[0] +
+        "</p>" +
+        "<p> Description: " +
+        data.results[i].description +
+        "</p>" +
+        "<a class='btn btn-dark mb-3' href='" +
+        data.results[i].url +
+        "' target='_blank' > Read More </a>" +
+        "</div>"
+    );
+  }
+  $(".loading").attr("class", "hide");
 };
 
-// EVENT LISTENERS
-
+// EVENT LISTENERS ---------------------------------------------------------------------------
 $("#submitBtn").on("click", function (event) {
-    event.preventDefault();
-    searchTerm = searchTermEl.val().trim();
-    formatInput = formatInputEl.val().trim();
-    if(searchTerm === ''){
-        alert('Please enter a search term');
-    }
-    if (formatInput == 'Select a format...') {
-      formatInput = 'newspapers'
-    }
-    var queryParameters = 'q=' + searchTerm + '&format=' + formatInput
-    location.replace('search-results.html?' + queryParameters);
+  event.preventDefault();
+  searchTerm = searchTermEl.val().trim();
+  formatInput = formatInputEl.val().trim();
+  if (searchTerm === "") {
+    alert("Please enter a search term");
+  }
+  if (formatInput == "Select a format...") {
+    formatInput = "newspapers";
+  }
+  var queryParameters = "q=" + searchTerm + "&format=" + formatInput;
+  location.replace("search-results.html?" + queryParameters);
 });
 
 $("#backBtn").on("click", function (event) {
